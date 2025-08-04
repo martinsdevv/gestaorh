c42fca34a68e403873098e08b7b0b538e8c6a813
martinsdevv <martins.devv@gmail.com>
2025-08-04 18:29:49 -0300

    fix README

--- GestaoRH.Web/obj/Debug/net8.0/GestaoRH.Web.AssemblyInfo.cs
+++ GestaoRH.Web/obj/Debug/net8.0/GestaoRH.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestaoRH.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba30bd6af7984391005223d7a800a1865b2188a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestaoRH.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestaoRH.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
